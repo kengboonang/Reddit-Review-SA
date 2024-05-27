bdbb69bb6e9b1777f12f4cfb61ee23297ea1ac86
Kay Bee <77047528+kengboonang@users.noreply.github.com>
2024-05-27 19:15:15 +0200

    Updating apify command on main.py & requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 
+from apify_client import ApifyClient
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from models.huggingface_base import analyze_sentiment
@@ -22,16 +23,33 @@ def scrape():
 def scrape_reviews(url):
     # Load the API key
     API_KEY = os.getenv('API_KEY')
-    API_URL = f'https://api.scrapingdog.com/scrape?api_key={API_KEY}&url={url}&render_js=true'
-    payload = {"startUrls": [{"url": url}]}
+    client = ApifyClient(api_key=API_KEY)
+
+    # Prepare the Actor input
+    run_input = {
+        "startUrls": [{ "url": url }],
+        "maxReviews": 100,
+        "language": "en",
+    }
+    run = client.actor("compass/google-maps-reviews-scraper").call(run_input=run_input)
+    print("💾 Check your data here: https://console.apify.com/storage/datasets/" + run["defaultDatasetId"])
+    
+    # Fetch data
+    dataset_items = client.dataset(run['defaultDatasetId']).list_items().items
+
+    # Extract reviews
+    reviews = [item['review'] for item in dataset_items]
+    
+    # API_URL = f'https://api.scrapingdog.com/scrape?api_key={API_KEY}&url={url}&render_js=true'
+    # payload = {"startUrls": [{"url": url}]}
 
     # Send a POST request to the API
-    response = requests.post(API_URL, json=payload)
-    if response.status_code == 200:
-        reviews = response.json()
-        return [review["text"] for review in reviews if "text" in review]
-    else:
-        return []
+    # response = requests.post(API_URL, json=payload)
+    # if response.status_code == 200:
+    #     reviews = response.json()
+    #     return [review["text"] for review in reviews if "text" in review]
+    # else:
+    #     return []
 
 if __name__ == '__main__':
     load_dotenv()
